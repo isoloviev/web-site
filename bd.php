5d374be587e817e9a3f4f74db61c0a166ac638e6
Ivan Soloviev <ivans@dins.ru>
2015-06-13 12:06:38 +0300

    Initial commit

---
+++ bd.php
@@ -0,0 +1,13 @@
+﻿<?php
+session_start();
+
+$link=mysql_connect("localhost",'u9823881_web','mwmiNrpw');
+mysql_select_db("u9823881_web",$link) or die("Не доступно");
+mysql_query("SET NAMES utf8");
+
+$login = $_SESSION['login'];
+$password = $_SESSION['password'];
+$id_user = $_SESSION['id'];
+
+
+print_r($_SESSION);
---
+++ verification.php
@@ -0,0 +1,47 @@
+﻿<?php
+    include_once("bd.php");
+	
+    if (isset($_POST['submit'])){
+		if(empty($_POST['password'])) {
+			echo '<br><font color="red"><img border="0" src="error.gif" align="middle" alt="Введите пароль !"> Введите пароль!</font>';
+		}
+		elseif (!preg_match("/\A(\w){6,20}\Z/", $_POST['password'])) {
+			echo '<br><font color="red"><img border="0" src="error.gif" align="middle" alt="Пароль слишком короткий!"> Пароль слишком короткий! Пароль должен быть не менее 6 символов! </font>';
+		}
+		elseif(empty($_POST['password2'])) {
+			echo '<br><font color="red"><img border="0" src="error.gif" align="middle" alt="Введите подтверждение пароля!"> Введите подтверждение пароля!</font>';
+		}
+		elseif($_POST['password'] != $_POST['password2']) {
+			echo '<br><font color="red"><img border="0" src="error.gif" align="middle" alt="Введенные пароли не совпадают!"> Введенные пароли не совпадают!</font>';
+		}
+		elseif(empty($_POST['email'])) {
+			echo '<br><font color="red"><img border="0" src="error.gif" align="middle" alt="Введите E-mail!">Введите E-mail! </font>';
+		}
+		elseif (!preg_match("/^[a-zA-Z0-9_\.\-]+@([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,6}$/", $_POST['email'])) {
+			echo '<br><font color="red"><img border="0" src="error.gif" align="middle" alt="E-mail имеет недопустимий формат!"> E-mail имеет недопустимий формат! Например, name@gmail.com! </font>';
+		}
+		 
+		else{
+			$password = $_POST['password'];
+			$mdPassword = md5($password);
+			$password2 = $_POST['password2'];
+			$email = $_POST['email'];
+			$name = $_POST['name'];
+
+
+				$query2 = ("SELECT email FROM subscribers WHERE email='$email'");
+				$sql = mysql_query($query2) or die(mysql_error());
+				if (mysql_num_rows($sql) > 0){
+					echo '<font color="red"><img border="0" src="error.gif"  alt="Пользователь с таким e-mail зарегистрированый!"> Пользователь с таким e-mail уже зарегистрирован!</font>';
+				}
+				else{
+					$query = "INSERT INTO subscribers (password, email, realname )
+							  VALUES ('$mdPassword', '$email', '$name')";
+					$result = mysql_query($query) or die(mysql_error());;
+					echo '<font color="green"><img border="0" src="ok.gif" align="middle" alt="Вы успешно зарегистрировались!"> Вы успешно зарегистрировались!</font><br><a href="index.php">На главную</a>';
+					
+								
+				}
+		}
+    }
+?>
